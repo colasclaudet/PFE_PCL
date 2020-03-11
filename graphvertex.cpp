29b688e5266fa56fe40496aad8a19797d7f41102
yoannfoulon <yoann.foulon@etu.univ-amu.fr>
2020-03-11 19:50:17 +0100

    Fin provisoire Kippi

--- graphvertex.cpp
+++ graphvertex.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 using namespace cv;
 
-GraphVertex::GraphVertex(int i, int j, Point2f intersection) : m_i(i), m_j(j), m_intersection(intersection) {}
+int GraphVertex::index() const
+{
+    return m_index;
+}
+
+void GraphVertex::setIndex(int index)
+{
+    m_index = index;
+}
+
+void GraphVertex::flipActive()
+{
+    m_active = 1 - m_active;
+}
+
+GraphVertex::GraphVertex(int i, int j, Point2f intersection, int index) : m_i(i), m_j(j), m_intersection(intersection) {}
+
+GraphVertex::GraphVertex() {}
+
+GraphVertex::GraphVertex(const GraphVertex &g) : m_i(g.i()), m_j(g.j()), m_intersection(g.intersection()) {}
+
 int GraphVertex::j() const
 {
     return m_j;
