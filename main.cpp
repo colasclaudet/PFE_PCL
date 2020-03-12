efba2cc3b3de052aa5f4cad7a72d1e89b8d07e7b
yoannfoulon <yoann.foulon@etu.univ-amu.fr>
2020-03-12 11:24:24 +0100

    Changement pour QVector2D

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 #include <QApplication>
 #include <QDebug>
 #include <utility>
-#include <vector>
-
+#include <QVector2D>
 //Commenter si non Kippi
 #include <opencv2/core/types.hpp> //Point2f
 
@@ -25,14 +24,14 @@ int main(int argc, char *argv[])
     Kippi k;
 
     //Ajouter un second paramètre "true" à partition pour afficher les images de Kippi
-    std::pair<std::vector<std::vector<cv::Point>>, std::vector<double>> p = k.partition(in);
-    std::vector<std::vector<cv::Point>> contours = p.first;
+    std::pair<std::vector<std::vector<QVector2D>>, std::vector<double>> p = k.partition(in, true);
+    std::vector<std::vector<QVector2D>> contours = p.first;
     std::vector<double> medianValues = p.second;
 
     for(int i = 0 ; i < contours.size() ; ++i ){
         for(int j = 0 ; j < contours[i].size() ; ++j ){
-            std::cout << "Contour " << i << ", Point " << j << ", X: " << contours[i][j].x <<
-                         ", Y: " << contours[i][j].y << std::endl;
+            std::cout << "Contour " << i << ", Point " << j << ", X: " << contours[i][j].x() <<
+                         ", Y: " << contours[i][j].y() << std::endl;
         }
 
         std::cout << "Contour " << i << ", Valeur médiane: " << medianValues[i] << std::endl << std::endl;
